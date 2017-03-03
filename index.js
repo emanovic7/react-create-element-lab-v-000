4907e8d6cd459e980aa738e9b9a82a9428f689f4
Emmanuel Owusu <e_owusu@live.com>
2017-03-03 20:13:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 //Code React element here
+
+/*const meInReact = React.createElement('div', {className: 'Me'},
+      React.createElement('h1', {}, 'An Awesome Pperson'),
+      Reach.createElement('p', {}, 'Who is learning React')
+      
+
+
+);
+
+ReactDOM.render(meInReact, 
+  document.getElementsById('main'));
+*/
+
+
+  const meInReact = React.createElement('div', {className: 'me'}, 
+      React.createElement('h1', {}, 'An Awesome Person'),
+      React.createElement('p', {}, 'Who is learning React'),
+
+      React.createElement('ul', {className: 'me__interests'},
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice cream'),
+    ),
+
+);
+   
+  ReactDOM.render(
+    meInReact,
+    document.getElementById('main')
+  );
